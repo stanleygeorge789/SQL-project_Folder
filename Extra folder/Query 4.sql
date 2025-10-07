d9bd17802264952c27ee56515ac3ae4f351af8dd
stanleygeorge789 <stanley.in.manit@gmail.com>
2025-10-08 00:13:38 +0530

    query push

--- Extra folder/Query 4.sql
+++ Extra folder/Query 4.sql
@@ -1,1 +1,16 @@
--- delete this later
+SELECT 
+    skills_dim.skills AS skill,
+    COUNT(*) AS demand_count
+FROM job_postings_fact
+INNER JOIN skills_job_dim 
+    ON job_postings_fact.job_id = skills_job_dim.job_id
+INNER JOIN skills_dim 
+    ON skills_job_dim.skill_id = skills_dim.skill_id
+WHERE 
+    job_title_short = 'Data Analyst'
+    -- AND job_work_from_home = TRUE   -- Uncomment this to filter remote jobs only
+GROUP BY 
+    skills_dim.skills
+ORDER BY 
+    demand_count DESC
+LIMIT 1000;
