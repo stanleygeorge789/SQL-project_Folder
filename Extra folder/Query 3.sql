dfdc069f5e4d815355724f2f7509556fdc006152
stanleygeorge789 <stanley.in.manit@gmail.com>
2025-10-07 23:55:02 +0530

    query 3 push

--- Extra folder/Query 3.sql
+++ Extra folder/Query 3.sql
@@ -1,1 +1,9 @@
--- delete this later
+SELECT
+    skills,
+    count(skills_job_dim.job_id) AS demand_count
+FROM job_postings_fact
+INNER JOIN skills_job_dim ON job_postings_fact.job_id = skills_job_dim.job_id
+INNER JOIN skills_dim ON skills_job_dim.skill_id = skills_dim.skill_id
+GROUP BY
+    skills
+LIMIT 1000
